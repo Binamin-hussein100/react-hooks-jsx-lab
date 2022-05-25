88525ead5273ae415436a5ac0c8e73702dba695a
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-05-25 18:01:07 +0300

    Complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div>
+    <h2>About me</h2>
+    <p>I am a Full stack software developer</p>
+    <img alt="i made this" src={image}></img>
+  </div>
+  )
 }
 
 export default About;
